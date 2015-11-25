53a73e06091c6b48aca62f21e18d1ae27e169f21
JC <santosjohncarlo96@gmail.com>
2015-11-25 14:22:05 +0800

    end of chapter 12

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -2,6 +2,7 @@ from.base import FunctionalTest
 
 
 class LayoutAndStylingTest(FunctionalTest):
+	
 	def test_layout_and_styling(self):
    		# Edith goes to the home page
    		self.browser.get(self.server_url)
